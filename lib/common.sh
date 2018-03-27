fd96b5c95c09041ca2cde009b4209dfd00dc7a9c
NipunaPrashan <nipunaprashan99@gmail.com>
2018-03-27 17:37:13 +0530

    Adding licence

--- lib/common.sh
+++ lib/common.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+# ----------------------------------------------------------------------------
+# Copyright (c) 2018 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+#
+# WSO2 Inc. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# ----------------------------------------------------------------------------
 
 export DEFAULT_MAVEN_VERSION="3.3.9"
 export BUILDPACK_STDLIB_URL="https://lang-common.s3.amazonaws.com/buildpack-stdlib/v7/stdlib.sh"
@@ -104,3 +121,4 @@ install_jdk() {
   install_java_with_overlay ${install_dir}
   mtime "jvm.install.time" "${start}"
 }
+
